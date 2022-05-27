d37000cd218f434a53bb428a3457fd9ffe8f475c
Oskar <oskar@pop-os.localdomain>
2022-05-28 04:27:10 +1000

    changed pool fee

--- pool.py
+++ pool.py
@@ -264,7 +264,7 @@ class pool:
 
         for address in addresses:
             block_reward = 10
-            pool_fee = 0.2
+            pool_fee = 0.35
             contributed = self.share_dict.get(address)
             print(address, "total shares: ", contributed)
             split = contributed / total_shares
@@ -353,13 +353,13 @@ def receive_proof():
         if Validation.validate_signature(public_key_hex, signature, trans_data):
             print("signature valid")
             pool.calculate_split()
-            if pool.send_proof(proof=proof, prev_proof=last_proof) == True:
+            if pool.send_proof(proof=proof, prev_proof=last_proof):
                 for address in pool.unpaid_rewards:
                     amount = pool.unpaid_rewards.get(address)
                     print(amount)
                     if amount >= 20:
                         pool.dispense_reward(address=address, amount=amount)
-                        return "ok", 200
+
                 return "ok", 200
             else:
                 print("stale proof submitted getting new proof")
